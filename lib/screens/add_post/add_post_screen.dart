c94f165b113ef1ba92fdbcbbcc5ce8ad9f7f0cba
roufy235 <roufy235@gmail.com>
2022-06-17 21:52:35 +0100

    feat: photo picker

--- lib/screens/add_post/add_post_screen.dart
+++ lib/screens/add_post/add_post_screen.dart
@@ -1,19 +1,72 @@
-import 'package:flutter/material.dart';
-import 'package:instagram_clone_app/utils/colors.dart';
+import 'dart:typed_data';
 
-class AddPostScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone_app/models/user.dart';
+import 'package:instagram_clone_app/providers/user_provider.dart';
+import 'package:instagram_clone_app/utils/colors.dart';
+import 'package:instagram_clone_app/utils/utils.dart';
+import 'package:provider/provider.dart';
+
+class AddPostScreen extends StatefulWidget {
   const AddPostScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    // return Center(
-    //   child: IconButton(
-    //     onPressed: () {},
-    //     icon: const Icon(Icons.upload),
-    //   ),
-    // );
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
 
-    return Scaffold(
+class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _selectedImage;
+  TextEditingController _descriptionController = TextEditingController();
+
+  void _selectImage(BuildContext context) async {
+    return showDialog(context: context, builder: (context) {
+      return SimpleDialog(
+        title: const Text('Create a post'),
+        children: [
+          SimpleDialogOption(
+            padding: const EdgeInsets.all(20),
+            child: const Text('Take a photo'),
+            onPressed: () async {
+              Navigator.of(context).pop();
+              Uint8List file = await pickImage(ImageSource.camera);
+              setState(() {
+                _selectedImage = file;
+              });
+            },
+          ),
+          SimpleDialogOption(
+            padding: const EdgeInsets.all(20),
+            child: const Text('Choose from gallery'),
+            onPressed: () async {
+              Navigator.of(context).pop();
+              Uint8List file = await pickImage(ImageSource.gallery);
+              setState(() {
+                _selectedImage = file;
+              });
+            },
+          ),
+          SimpleDialogOption(
+            padding: const EdgeInsets.all(20),
+            child: const Text('Cancel'),
+            onPressed: () => Navigator.of(context).pop(),
+          )
+        ],
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final UserModel userModel = Provider.of<UserProvider>(context).getUser;
+
+    return _selectedImage == null ? Center(
+      child: IconButton(
+        onPressed: () => _selectImage(context),
+        icon: const Icon(Icons.upload),
+      ),
+    ) : Scaffold(
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
         leading: IconButton(
@@ -43,11 +96,12 @@ class AddPostScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               CircleAvatar(
-                backgroundImage: NetworkImage("https://picsum.photos/200/300"),
+                backgroundImage: NetworkImage(userModel.photoUrl),
               ),
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.5,
                 child: TextField(
+                  controller: _descriptionController,
                   decoration: InputDecoration(
                     hintText: "write a caption...",
                     border: InputBorder.none
@@ -63,7 +117,7 @@ class AddPostScreen extends StatelessWidget {
                   child: Container(
                      decoration: BoxDecoration(
                        image: DecorationImage(
-                         image: NetworkImage("https://picsum.photos/200/300"),
+                         image: MemoryImage(_selectedImage!),
                          fit: BoxFit.fill,
                          alignment: FractionalOffset.topCenter
                        ),
